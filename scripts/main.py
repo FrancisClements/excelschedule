4e2a798f4b765f76a5fccba1947673eda1d53acd
Francis Clemente <francisclemente727@yahoo.com.ph>
2020-06-30 17:34:40 +0800

    Inital

---
+++ scripts/main.py
@@ -0,0 +1,18 @@
+import schedule as s
+
+#List of things on the table:
+    #CLASS CODE
+    #CODE
+    #SECTION
+    #SUBJECT NAME
+    #UNITS
+    #TIME-IN
+    #TIME-OUT
+    #DAYS
+    #ROOM
+
+print('Hello Yatsu')
+schedule = s
+
+if __name__ == "__main__":
+    print('This is run directly')
---
+++ scripts/schedule.py
@@ -0,0 +1,5 @@
+import pandas as pd, xlsxwriter as xl
+
+class SchedMaker:
+    def ___init__(self):
+        pass
