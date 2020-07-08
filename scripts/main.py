0b6d1410fb737f76f1fe43ecc522032a2a96cc59
Francis Clemente <francisclemente727@yahoo.com.ph>
2020-07-08 13:13:25 +0800

    Added .ttk + Fixes

--- scripts/main.py
+++ scripts/main.py
@@ -35,13 +35,13 @@ class MainMenu(Frame):
         title_k = Kinter(f, widget_list)
 
         #things to write
-        title_k.label('Excel Schedule Maker', font_size = 'header')
+        title_k.label('Excel Schedule Maker', theme = 'header')
         title_k.label('Create a graphical version of your schedule using Excel')
         title_k.label('Insert the schedule file and name the output file.')
 
         for widget in widget_list:
             title_k.widget_pack(widget, fill_wid = 'both')
- 
+
     def file_form(self):
         f = Frame(height = 100, width = 100, master = self)
         f.pack(expand = 1)
@@ -53,12 +53,12 @@ class MainMenu(Frame):
         input_frame = Kinter(title_k.labelframe('Input File', padding = [0, 5]), input_wid_list)
         input_txt = StringVar()
 
-        input_form = input_frame.entry(width = 40, read_only = 'readonly')
-        input_form.config(textvariable = input_txt, readonlybackground = LIGHT_GREY1)
+        input_form = input_frame.entry(width = 40, read_only = 1)
+        input_form.config(textvariable = input_txt)#, readonlybackground = LIGHT_GREY1)
 
         i_btn = input_frame.button('Browse', size = 'form', 
                     cmd = lambda x = input_txt: self.browse(x))
-        i_btn.config(bg = LIGHT_GREY2)
+        # i_btn.config(bg = LIGHT_GREY2)
 
         #Output File
         out_wid_list = []
@@ -66,7 +66,7 @@ class MainMenu(Frame):
         self.out_txt = StringVar()
 
         out_form = out_frame.entry(width = 40)
-        out_form.config(textvariable = self.out_txt, bg = LIGHT_GREY1)
+        out_form.config(textvariable = self.out_txt)#, bg = LIGHT_GREY1)
         out_frame.label('.xlsx')
         out_frame.label('The output file is at the same location as the input file.')
 
@@ -79,7 +79,7 @@ class MainMenu(Frame):
 
         #output form
         out_frame.widget_grid(out_wid_list[0], pos = [0,0])
-        out_frame.widget_grid(out_wid_list[1], pos = [1,0], snap=W)
+        out_frame.widget_grid(out_wid_list[1], pos = [1,0])
         out_frame.widget_grid(out_wid_list[2], pos = [0,1], span=[2,1], padding = [4,0])
 
     def next_warning(self):
@@ -90,16 +90,17 @@ class MainMenu(Frame):
         last_wid_list = []
         last_frame = Kinter(f, last_wid_list)
         warning_txt = StringVar(value = '')
-        warning_lbl = last_frame.label('')
-        warning_lbl.configure(textvariable = warning_txt, width = 33,
-                    anchor = W, fg = RED)
+        warning_lbl = last_frame.label('', theme = 'warning')
+        warning_lbl.configure(textvariable = warning_txt, width = 40,
+                    anchor = W)
         
         next_btn = last_frame.button('Next',
                 cmd = lambda x = warning_txt: self.check_form(x))
-        next_btn.config(bg = LIGHT_GREY2)
+        # next_btn.config(bg = LIGHT_GREY2)
 
         #render
-        last_frame.widget_grid(last_wid_list[0], pos = [0,0], span = [2,1], padding = [7,5], snap = W)
+        last_frame.widget_grid(last_wid_list[0], pos = [0,0], span = [2,1],
+                    padding = [7,5], snap = W)
         last_frame.widget_grid(last_wid_list[1], pos = [2,0], padding = [10,5])
 
     def check_form(self, txt_var):
@@ -113,6 +114,10 @@ class MainMenu(Frame):
         if all(form_check):
             txt_var.set('Enter the Input File and Output Filename')
             return
+        #checks the naming of the filename
+        elif re.search(str_format, output_file):
+            txt_var.set('Improper filename')
+            return
         elif any(form_check):
             print('one of them')
             warning_txt = 'Enter the '
@@ -120,10 +125,7 @@ class MainMenu(Frame):
             txt_var.set(warning_txt)
             return
 
-        #checks the naming of the filename
-        if re.search(str_format, output_file):
-            txt_var.set('Filename is not allowed')
-            return
+
 
         #All conditions are met
         self.allow_next = 1
@@ -161,60 +163,131 @@ class Options(Frame):
     def __init__(self, master = None):
         self.data = data
         self.master = master
-        self.main_k = Kinter(self)
+        self.frames = []
+        self.main_k = Kinter(self, self.frames)
         super().__init__(master.root, bg = LIGHT_GREY1)
+
+        self.state = {}
+        for option in ['hour_list', 'header', 'name']:
+            #value to true on hour list and header
+            self.state[option] = BooleanVar(value = True) if option != 'name' else BooleanVar()
 
     def render(self):
         self.pack(fill = 'both', expand = 1, pady = 50, padx = 10)
-        f = Frame(height = 100, width = 100, master = self)
-        f.pack(expand = 1)
-        options_list = []
-        options_frame = Kinter(f, options_list)
 
-        #setting up option states
-        state = {}
-        for option in ['hour_list', 'header', 'name']:
-            state[option] = BooleanVar()
+        '''
+        creates 2 frames (left and right) and then render it using grid
+        all widgets inside of those 2 frames will be packed.
+        '''
 
-        options_frame.label('Customize', font_size = 'header')
-        options_frame.checkbox('Enable Hour List', var = state['hour_list'])
-        options_frame.label('Day Format')
-        options_frame.label('Time Format')
-        options_frame.checkbox('Enable Header', var = state['header'])
-        options_frame.checkbox('Add your name to the schedule', var = state['name'])
-        # options_frame.button('Enter')
+        # rendering the frames
+        title = self.main_k.label('Customize', theme = 'header')
+        self.main_k.widget_grid(title, pos = [0,0], span = [2,1], snap = W)
 
-        for i, widget in enumerate(options_list):
-            if i == 0:
-                cell = [0,0]
-            elif isinstance(widget, Button):
-                cell = [1, (i-1)//2+2]
-                options_frame.widget_grid(widget, pos = cell)
-                continue
+        self.main_k.widget_grid(self.left_frame(), pos = [0,1], snap = NW)
+        self.main_k.widget_grid(self.right_frame(), pos = [1,1], snap = NW)
 
-            else:
-                cell = [(i-1)%2, (i-1)//2+1]
+        
+    def left_frame(self):
+        widgets = []
+        f = Frame(height = 100, width = 100, master = self, pady = 5)
+        frame = Kinter(f, widgets)
 
-            options_frame.widget_grid(widget, pos = cell, snap = W, padding = [10,0])
+        #widgets
+        frame.checkbox('Enable Hour List', var = self.state['hour_list'])
+        frame.checkbox('Enable Header', var = self.state['header'])
+        frame.entry()
+        frame.checkbox('Include Name', var = self.state['name'])
+        frame.entry(read_only = 1)
+
+        #tooltip descriptions
+        desc = [
+            'Adds a list of hour time in your schedule\n' '(adds more organization)',
+            'Add a header or title',
+            'Add your name to your schedule'
+        ]
+
+        #listed self.satate keys that uses an entry
+        entry_states = ['header', 'name']
+        desc_index = 0
+        for i, widget in enumerate(widgets):
+            #if the widget is a checkbox
+            if isinstance(widget, ttk.Checkbutton):
+                #command that changes the state of the input form
+                if desc_index >= 1:
+                    widget.config(command = lambda x = self.state[entry_states[desc_index-1]],
+                        y = widgets[i+1]: self.set_entry(x,y))
+
+                #adds tooltip
+                tooltip(widget, desc[desc_index])
+                desc_index += 1
+            
+            #spacing and render
+            frame.widget_grid(widget, pos = [0,i], padding = [10,0], snap = W)
+
+        #tooltip text
+        tooltip(widgets[0], 'Adds a list of hour time in your schedule\n'
+                        '(adds more organization)')
+        tooltip(widgets[1], 'Add a header or title')
+        tooltip(widgets[3], 'Add your name to your schedule')
+
+        return f
 
 
-    def get_state(self, var):
-        print(var.get())
+    def right_frame(self):
+        widgets = []
+        f = Frame(height = 100, width = 100, master = self, pady = 5)
+        frame = Kinter(f, widgets)
+
+        #list of options/vars
+        day_formats = ['Initial', 'Partial', 'Full']
+        time_formats = ['12hr + AM/PM', '12hr + a/p', '24hr']
+
+        day_var = StringVar()
+        time_var = StringVar()
+
+        #widgets
+        frame.label('Day Format') #e.g. Mon, Monday, M
+        frame.dropdown(day_formats, 1)
+        frame.label('Time Format')
+        frame.dropdown(time_formats, 0)
+
+
+        #fixing spacing of the widgets + render
+        for i, widget in enumerate(widgets):
+            frame.widget_grid(widget, pos = [0,i], padding = [10,0], snap = W)
+
+        return f
+
+    def set_entry(self, var, widget):
+        #if the button is unchecked, the input form will disable.
+        #var = true/false, widget = entry widget
+        if not var.get():
+            widget.configure(style = 'Disable.TEntry', state = DISABLED)
+        else:
+            widget.configure(style = 'TEntry', state = NORMAL)
+        
+
+
+
+
+    def get_status(self, states):
+        for key, val in zip(states, states.values()):
+            print(key, val.get())
 
 #program window
 class Program:
-    def __init__(self):
+    def __init__(self, mode = None):
         self.root = Tk()
         self.root.title('Excel Schedule Maker')
         self.root.geometry('600x400')
         self.new()
-        self.run('test')
+        self.run(mode)
         print('App Exit')
 
     def new(self):
         #puts all frames in a list
         self.frames = [0, [MainMenu(self), Options(self)]]
-        print(self.frames)
 
     def next_frame(self):
         #hides the current frame, shows the next frame
@@ -233,13 +306,13 @@ class Program:
         # print(self.root.pack_slaves())
 
         #main loop of the program
-        self.root.mainloop()
-    
+        self.root.mainloop() 
+
     def create_schedule(self):
         schedule.create_schedule()
 
 if __name__ == "__main__":
-    p = Program()
+    p = Program('test')
 
     # r = Tk()
     # r.geometry("800x400")
--- scripts/widgets.py
+++ scripts/widgets.py
@@ -1,7 +1,180 @@
 from tkinter import *
 from datetime import datetime as dt
 import tkinter.ttk as ttk, tkinter.font as tkfont, json, os
-from tkcolorpicker import askcolor
+from tkcolorpicker import askcolor  
+
+#initializing the methods for tkinter
+'''
+    Kinter class is basically a cheat-sheet for
+    TKinter. This class helps reduce the line of code
+    to the main.py script.
+
+    Parameters:
+    root - a frame where you can insert widgets such as labels, buttons
+    widget_list - collects created widgets for easy rendering.
+'''
+class Kinter:
+    def __init__(self, root, widget_list = None):
+        self.root = root
+        self.style = ttk.Style()
+        self.widget_list = widget_list
+        self.config_style()
+
+    def config_style(self):
+        #label formats: header, warning
+        size = FONT_SIZE['header']
+        self.style.configure('Header.TLabel', font = f'Corbel {size} bold')
+        self.style.configure('Warning.TLabel', foreground = RED)
+
+        #button formats: form
+        #button modified: size
+        padding = [str(x) for x in BUTTON_SIZE['default'].values()]
+        padding = ' '.join(padding)
+        self.style.configure('Form.TButton', padding = '0 0')
+        self.style.configure('TButton', padding = f'{padding}')
+
+        #entry modified: disabled
+        self.style.layout('Disable.TEntry', [('Entry.plain.field', {'children': [(
+                       'Entry.background', {'children': [(
+                           'Entry.padding', {'children': [(
+                               'Entry.textarea', {'sticky': 'nswe'})],
+                      'sticky': 'nswe'})], 'sticky': 'nswe', 'border': '20'})],
+                      'border':'20', 'sticky': 'nswe'})] )
+
+        # self.style.configure("Disable.TEntry",
+        #          background='red', 
+        #          fieldbackground = f'{LIGHT_GREY1}', relief = FLAT)
+
+        self.style.map('Disable.TEntry',
+            foreground = [('disabled', 'black')],
+            fieldbackground = [('!disabled', 'red'), ('disabled', f'{LIGHT_GREY1}' )])
+
+
+    def add_to_list(self, widget):
+        #adds widgets to the list
+        if self.widget_list != None:
+            self.widget_list.append(widget)
+        return widget
+
+    def label(self, item, theme = 'default'):
+        #initializes the label
+        l = ttk.Label(self.root, text = item)
+
+        if theme == 'header':
+            l.configure(style = 'Header.TLabel')
+        elif theme == 'warning':
+            l.configure(style = 'Warning.TLabel')
+
+        return self.add_to_list(l)
+        
+    def button(self, item, size = 'default', font_size = 'default', state_type = None, cmd = None):
+
+        #initializes the button
+        b = ttk.Button(self.root, text = item, state = state_type, command = cmd)#, font = used_font)
+
+        #sets button size
+        if size == 'form':
+            b.config(style = 'Form.TButton')
+        
+        # if isinstance(size, str):
+        #     b['padx'] = BUTTON_SIZE[size]['x']
+        #     b['pady'] = BUTTON_SIZE[size]['y']
+        # else:
+        #     b['padx'], b['pady'] = size
+        return self.add_to_list(b)
+
+    def entry(self, width = 20, read_only = 0):
+        #initializes entry
+        e = ttk.Entry(self.root)
+        e['width'] = width
+
+        #sets the state of the entry.
+        if isinstance(read_only, int) or isinstance(read_only, bool):
+            e['state'] = DISABLED if read_only else NORMAL
+            e['style'] = 'Disable.TEntry' if read_only else None
+        else:
+            e['state'] = read_only
+
+        return self.add_to_list(e)
+
+    def labelframe(self, title, font_size = 'default', padding = [0,0]):
+        #sets size
+        used_font = self.set_font_size(font_size)
+
+        #initializes the label frame
+        lf = ttk.LabelFrame(self.root, text = title)#, 
+            #padx = padding[0], pady = padding[1], font = used_font)
+        return self.add_to_list(lf)
+
+    def checkbox(self, item, var = None, cmd = None):
+        cb = ttk.Checkbutton(self.root, text = item,
+            command = cmd, variable = var)
+        return self.add_to_list(cb)
+
+    def dropdown(self, item, val = 0, cmd = None):
+        drop = ttk.Combobox(self.root, value = item)
+        drop.current(val)
+        return self.add_to_list(drop)
+
+    #non-widget methods
+    def widget_pack(self, widget, padding = [0,0], fill_wid = None, expand_wid = None, snap = None):
+        #renders array of pack()
+        widget.pack(padx = padding[0], pady = padding[1])
+        widget.pack_configure(side = snap)
+        widget.pack_configure(expand = expand_wid, fill = fill_wid)
+
+    def widget_grid(self, widget, pos = [0,0], span = [1,1], padding = [0,0], snap = None):
+        #renders array of grid()
+        widget.grid(row = pos[1], column = pos[0])
+        widget.grid_configure(padx = padding[0], pady = padding[1])
+        widget.grid_configure(columnspan = span[0], rowspan = span[1])
+        widget.grid_configure(sticky = snap)
+
+'''class that creates a tooltip when a widget is hovered'''
+
+class ToolTip(object):
+    def __init__(self, widget):
+        self.widget = widget
+        self.tipwindow = None
+        self.id = None
+        self.x = self.y = 0
+
+    def showtip(self, text):
+        "Display text in tooltip window"
+        self.text = text
+        if self.tipwindow or not self.text:
+            return
+
+        x, y, cx, cy = self.widget.bbox("insert")
+        x = x + self.widget.winfo_rootx() + self.widget.winfo_width() + 10
+        y = y + cy + self.widget.winfo_rooty() + 3
+
+        #height pattern (15n+10): 25, 40, 55, 70
+        #3, 12, 18
+        #correct pattern: 3, 11, 18.5, 26.5
+
+        self.tipwindow = tw = Toplevel(self.widget)
+        tw.wm_overrideredirect(1)
+        tw.wm_geometry("+%d+%d" % (x, y))
+        label = Label(tw, text=self.text, justify=LEFT,
+                      background="#ffffe0", relief=SOLID, borderwidth=1)
+        label.pack(ipadx=1)
+
+    def hidetip(self):
+        tw = self.tipwindow
+        self.tipwindow = None
+        if tw:
+            tw.destroy()
+
+def tooltip(widget, text):
+    toolTip = ToolTip(widget)
+    def enter(event):
+        toolTip.showtip(text)
+    def leave(event):
+        toolTip.hidetip()
+
+    widget.bind('<Enter>', enter)
+    widget.bind('<Leave>', leave)
 
 #gathering global variables for tkinter
 def load_settings():
@@ -33,113 +206,8 @@ def write_data():
         json.dump(data, f, indent=4, sort_keys=True)
         f.truncate()
 
-load_settings()         
+load_settings()
 
-#initializing the methods for tkinter
-'''
-    Kinter class is basically a cheat-sheet for
-    TKinter. This class helps reduce the line of code
-    to the main.py script.
-
-    Parameters:
-    root - a frame where you can insert widgets such as labels, buttons
-    widget_list - collects created widgets for easy rendering.
-'''
-class Kinter:
-    def __init__(self, root, widget_list = None):
-        self.root = root
-        self.font = tkfont.Font()
-        self.widget_list = widget_list
-
-    def add_to_list(self, widget):
-        #adds widgets to the list
-        if self.widget_list != None:
-            self.widget_list.append(widget)
-        return widget
-
-    def set_font(self, font_pack):
-        #configures font
-        self.font = tkfont.Font(family = font_pack[0], size = font_pack[1])    
-        FONT_SIZE['default'] = font_pack[1]
-
-    def label(self, item, font_size = 'default'):
-        #sets size
-        used_font = self.set_font_size(font_size)
-                
-        #initializes the label
-        l = Label(self.root, text = item, font = used_font)
-        return self.add_to_list(l)
-        
-    def button(self, item, size = 'default', font_size = 'default', state_type = None, cmd = None):
-        #sets size
-        used_font = self.set_font_size(font_size)
-
-        #initializes the button
-        b = Button(self.root, text = item, state = state_type, command = cmd, font = used_font)
-
-        #sets button size
-        if isinstance(size, str):
-            b['padx'] = BUTTON_SIZE[size]['x']
-            b['pady'] = BUTTON_SIZE[size]['y']
-        else:
-            b['padx'], b['pady'] = size
-        return self.add_to_list(b)
-
-    def entry(self, width, bg = None, read_only = 0, font_size = 'default'):
-        #sets size
-        used_font = self.set_font_size(font_size)
-
-        #initializes entry
-        e = Entry(self.root)
-        e['width'] = width
-        e['bg'] = bg
-
-        #sets the state of the entry.
-        if isinstance(read_only, int):
-            e['state'] = DISABLED if read_only else NORMAL
-        else:
-            e['state'] = read_only
-
-        return self.add_to_list(e)
-
-    def labelframe(self, title, font_size = 'default', padding = [0,0]):
-        #sets size
-        used_font = self.set_font_size(font_size)
-
-        #initializes the label frame
-        lf = LabelFrame(self.root, text = title, 
-            padx = padding[0], pady = padding[1], font = used_font)
-        return self.add_to_list(lf)
-
-    def checkbox(self, item, var = None, cmd = None, font_size = 'default'):
-        #sets sizes
-        used_font = self.set_font_size(font_size)
-
-        cb = Checkbutton(self.root, text = item, 
-            variable = var, font = used_font, command = cmd)
-        return self.add_to_list(cb)
-
-    #non-widget methods
-    def widget_pack(self, widget, padding = [0,0], fill_wid = None, expand_wid = None, snap = None):
-        #renders array of pack()
-        widget.pack(padx = padding[0], pady = padding[1])
-        widget.pack_configure(side = snap)
-        widget.pack_configure(expand = expand_wid, fill = fill_wid)
-
-    def widget_grid(self, widget, pos = [0,0], span = [1,1], padding = [0,0], snap = None):
-        #renders array of grid()
-        widget.grid(row = pos[1], column = pos[0])
-        widget.grid_configure(padx = padding[0], pady = padding[1])
-        widget.grid_configure(columnspan = span[0], rowspan = span[1])
-        widget.grid_configure(sticky = snap)
-
-    def set_font_size(self, font_size):
-        #sets custom font size for widgets
-        new_font = self.font.copy()
-
-        if isinstance(font_size, str):
-            new_font.config(size = FONT_SIZE[font_size])
-        else:
-            new_font.config(size = font_size)
-
-        return new_font
+if __name__ == "__main__":
+    root = Tk()
+    Kinter(root)
